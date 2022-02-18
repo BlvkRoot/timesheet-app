773c04566eff006307dd545f2daf572d2a5c5b22
blvkroot <hsalukamba17@gmail.com>
2022-02-19 00:33:55 +0100

    Storing using username and password state for login

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,31 +1,55 @@
-
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useMutation } from 'react-query';
 import '../styles/Login.css';
 
 function LoginComponent() {
-    const handleLoginFormSubmit = (e) => {
-        e.preventDefault();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-        console.log('Yaraa Yara Yara/............');
+  const { isLoading, isSuccess, isError, mutate } = useMutation(
+    (loginParams) => {
+        console.log('Login params', loginParams);
+      return axios.post(
+        'http://localhost:4141/api/v1/users/login',
+        loginParams
+      );
     }
+  );
   return (
     <>
-        <div className='login__container'>
-            <form onSubmit={handleLoginFormSubmit}>
-                <h1>LimitedTimeTime</h1>
-                <div className='input__field'>
-                    <input type='text' className='username'/>
-                </div>
-                <div className='input__field'>
-                    <input type='password' className='password'/>
-                </div>
-                <div className='input__field'>
-                    <button type='submit'>Login</button>
-                </div>
-            </form>
-        </div>
+      <div className='login__container'>
+        <form
+          onSubmit={(e) => {
+              e.preventDefault();
+            mutate({
+              email: username,
+              password,
+            });
+          }}
+        >
+          <h1>LimitedTimeTime</h1>
+          <div className='input__field'>
+            <input
+              type='text'
+              className='username'
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div className='input__field'>
+            <input
+              type='password'
+              className='password'
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <div className='input__field'>
+            <button type='submit'>Login</button>
+          </div>
+        </form>
+      </div>
     </>
-  )
+  );
 }
 
-export default LoginComponent
+export default LoginComponent;
